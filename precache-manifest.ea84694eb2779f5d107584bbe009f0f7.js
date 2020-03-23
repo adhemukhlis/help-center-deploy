429a54c05dd499f748b2e7f618135f23dc1f2d2b
adhemukhlis <adhemukhlis@gmail.com>
2020-03-23 16:46:55 +0700

    init

---
+++ precache-manifest.ea84694eb2779f5d107584bbe009f0f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeeb11fd0acd559448e85fa3b7bdb0d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c96d418d4c9924794bee",
+    "url": "/static/css/2.f5d46098.chunk.css"
+  },
+  {
+    "revision": "099e0632e7824f6baa64",
+    "url": "/static/css/main.5c53a00e.chunk.css"
+  },
+  {
+    "revision": "c96d418d4c9924794bee",
+    "url": "/static/js/2.d0829549.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.d0829549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "099e0632e7824f6baa64",
+    "url": "/static/js/main.482cb8c5.chunk.js"
+  },
+  {
+    "revision": "ac81d3af9b4054313e72",
+    "url": "/static/js/runtime-main.75474000.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
